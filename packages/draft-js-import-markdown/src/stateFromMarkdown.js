9c58a16b18b85fa466f96e0691fa13a733625c69
Oliver Bolton <obolton@hugo.ai>
2018-06-05 14:37:14 -0700

    Fixed import

--- packages/draft-js-import-markdown/src/stateFromMarkdown.js
+++ packages/draft-js-import-markdown/src/stateFromMarkdown.js
@@ -1,10 +1,10 @@
 // @flow
 
 import MarkdownParser from './MarkdownParser';
-import {stateFromElement} from 'draft-js-import-element';
+import {stateFromElement} from '@hugoai/draft-js-import-element';
 
 import type {ContentState} from 'draft-js';
-import type {ElementStyles, CustomBlockFn, CustomInlineFn} from 'draft-js-import-element';
+import type {ElementStyles, CustomBlockFn, CustomInlineFn} from '@hugoai/draft-js-import-element';
 
 type Options = {
   elementStyles?: ElementStyles;
